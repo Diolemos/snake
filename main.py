f43aecdcdfc62133267c9286c40eb753f0384628
Dio Lemos <pedro.devsc@outlook.com>
2023-05-18 19:10:38 -0300

    create instence of Screen set up screen width, hwight, bgcolor, title set tracer to 0, create game_is_on boolean

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from snake import Snake
 import time
 from turtle import Screen
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake")
+screen.tracer(0)
+game_is_on = True 
