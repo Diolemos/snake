2f472f74f7a651a8636675f72fd41a57b78ff041
Dio Lemos <pedro.devsc@outlook.com>
2023-05-18 20:23:06 -0300

    get the snake to move using setheading

--- snake.py
+++ snake.py
@@ -3,7 +3,7 @@ class Snake:
     def __init__(self):
         self.body = []
         self.create_body()
-        
+        self.head = self.body[0]       
     def create_body(self):
         x_axis = 0
         for _ in range(3):
@@ -20,4 +20,17 @@ class Snake:
             new_y = self.body[index -1].ycor() 
             self.body[index].goto(new_x,new_y) 
         self.body[0].fd(20)     
+    
+    
+    def up(self):
+        self.head.setheading(90)
         
+    def down(self): 
+        self.head.setheading(270)
+    
+    def  left(self):  
+        self.head.setheading(180)    
+        
+    def right(self): 
+        self.head.setheading(00)        
+            
