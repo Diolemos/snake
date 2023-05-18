9224166d68031b06e8b51cf981b10c920dd6c28f
Dio Lemos <pedro.devsc@outlook.com>
2023-05-18 17:16:07 -0300

    create snake class it has a body list property, create_body function and move function move function moves body first and head last

---
+++ main.py
@@ -0,0 +1,4 @@
+from snake import Snake
+import time
+from turtle import Screen
+
---
+++ snake.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+class Snake:
+    def __init__(self):
+        self.body = []
+        
+    def create_body(self):
+        x_axis = 0
+        for _ in range(3):
+            new_segment = Turtle(shape='square')
+            new_segment.color('white')
+            new_segment.penup()
+            new_segment.goto(x=x_axis,y=0)
+            self.body.append(new_segment)
+            x_axis += 20
+        
+    def move(self):
+        for index in range(len(self.body)-1,0,-1):
+            new_x = self.body[index -1].xcor()
+            new_y = self.body[index -1].ycor() 
+            self.body[index].goto(new_x,new_y) 
+        self.body[0].fd(20)     
+        
