2ddc62b77278f8790602229548cc497ac6846fcc
Dio Lemos <pedro.devsc@outlook.com>
2023-05-18 19:26:22 -0300

    import snake create while game is on loop

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 game_is_on = True 
+
+snake = Snake()
+snake.create_body()
+
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
+    snake.move()
+
+
+
+
+
+screen.exitonclick()    
