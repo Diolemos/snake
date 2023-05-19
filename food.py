ce1a57573199cea163675474b3c3bfc9e46c12d7
Dio Lemos <pedro.devsc@outlook.com>
2023-05-18 22:55:19 -0300

    create food class

---
+++ food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+'''
+The Food class represents the food object in the game.
+It extends the Turtle class to inherit its attributes and methods.
+'''
+class Food(Turtle):
+    
+    def __init__(self):
+        super().__init__() #call the parent's init() class
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color('red')
+        self.speed('fastest')
+        random_x = random.randint(-280,280) #Pop up in a random location
+        random_y = random.randint(-280,280)
+        self.goto(random_x,random_y)
